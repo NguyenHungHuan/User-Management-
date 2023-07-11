3027910196127aad4acd9feb95f83c1788ba072f
Nguyen Hung Huan <nguyenhunghuan20062002@gmail.com>
2023-07-12 02:46:42 +0700

    init project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,11 @@
 /* eslint-env node */
 
 module.exports = {
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    'eslint-config-prettier', 'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
@@ -16,12 +18,26 @@ module.exports = {
     project: true,
     tsconfigRootDir: __dirname,
   },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh','prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     '@typescript-eslint/no-non-null-assertion': 'off',
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ]
   },
 }
